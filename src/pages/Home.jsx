3e8f6d3d6329769e6bb5ee14005b6d923ea4e8c0
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-01-02 17:28:24 +0530

    created input and fetched results from API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React , {useState} from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+// ,{ useState }
 
 const Home = () => {
-  return (
-    <MainPageLayout>   HOME PAGE</MainPageLayout>
-  )
-}
+  const [input, setInput]=useState('');
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
 
-export default Home
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=men
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>console.log(result))
+    // console.log(input);
+  };
+
+  const onKeyDown = (ev)=> {
+    // if the key pressed was Enter button //get keycodes from https://www.toptal.com/developers/keycode
+    if (ev.keyCode === 13) 
+    {
+      onSearch()
+    }
+  }
+
+  return (
+    <MainPageLayout>
+      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown}/>
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
+
+export default Home;
