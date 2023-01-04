3fb8fbc078f9fd4fe3827d353e210a267ebc8059
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-01-04 16:45:18 +0530

    installing styled componenets and creating styles for card

---
+++ src/components/actor/ActorCard.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+import { SearchCard } from '../styled';  
+// we will extend styles from searchcard
+
+export const StyledActorCard = styled(SearchCard)`
+  .deathday {
+    margin: 0;
+    margin-top: 15px;
+    font-weight: bold;
+  }
+`;
+
+
+
+
--- src/components/actor/ActorGrid.jsx
+++ src/components/actor/ActorGrid.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ActorCard from './ActorCard';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
 
 const Actor = ({data}) => {
   return (
-    <div>
+    <FlexGrid>
       {
         data.map(({person})=>(<ActorCard key={person.id} name={person.name} country={person.country?person.country.name:null} birthday={person.birthday} deathday={person.deathday} gender={person.gender} image={person.image?person.image.medium:IMAGE_NOT_FOUND}/>))
       }
-    </div>
+    </FlexGrid>
   )
 }
 
