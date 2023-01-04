4a6be954d53509e322e82dc58e2b62e1161b1adb
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-01-04 22:53:34 +0530

    replaced useStae with useReducer for managing states in Show pages

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,13 +1,34 @@
-import React , {useEffect, useState} from 'react';
+import React , {useEffect, useReducer} from 'react';
 import {useParams} from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
+const reducer = (prevstate,action)=>{
+    switch(action.type)
+    {
+        case 'FETCH_SUCCESS':{return {...prevstate,isLoading:false, show: action.show,error:null}}
+        // things written after ...prevstate will be overwritten in the prevstate because key names are same
+        case 'FETCH_FAILED':{return {...prevstate, isLoading:false, error: action.error}}
+        default: return prevstate
+    }
+}
+
+const initialState = {
+    show:null,
+    isLoading: true,
+    error: null
+}
+
+
+
 const Show = () => {
     const {id} = useParams();
-    const [show, setShow]=useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError]=useState(null);
-    // console.log(params,'params');
+    const [{show,isLoading,error},dispatch]=useReducer(reducer, initialState);
+    // console.log('state',state);
+    // const [{show,isLoading,error},dispatch]=useReducer(reducer, initialState);
+    // const [show, setShow]=useState(null);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [error, setError]=useState(null);
+
     useEffect(()=>{
         let isMounted =true;
 
@@ -15,16 +36,18 @@ const Show = () => {
             // setTimeout(() => {
                 if (isMounted)
                 {
-                    setShow(results);
-                    setIsLoading(false);
+                    dispatch({type:'FETCH_SUCCESS',show:results})
+                    // setShow(results);
+                    // setIsLoading(false);
                 }
                 
             // }, 2000);
         }).catch((err)=>{
             if (isMounted)
             {
-                setError(err.message);
-                setIsLoading(false);
+                dispatch({type:'FETCH_FAILED',show:err.message})
+                // setError(err.message);
+                // setIsLoading(false);
             }
             
         });
